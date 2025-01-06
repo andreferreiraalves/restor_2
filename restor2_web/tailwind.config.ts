15f6f4d373c48bfd1f25bf762a12387ab5110300
André Ferreira <andre.ferreira@mercatustecnologia.com.br>
2025-01-06 00:27:31 -0300

    starter commit

---
+++ restor2_web/tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+import formsPlugin from '@tailwindcss/forms';
+import prelinePlugin from 'preline/plugin';
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    formsPlugin,
+    prelinePlugin,],
+} satisfies Config;
